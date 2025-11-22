0ce625f561ebe75de71beb378e6862c69ff8fcb6
Federico <133919977+FedericoCWW@users.noreply.github.com>
2025-11-22 19:25:44 -0300

    21_11_2025_2

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,32 @@
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import '../features/Sidebar.scss'
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import "../features/Sidebar.scss";
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from './SidebarChannel.js'
 
 function Sidebar() {
   return (
-    <div className='sidebar'>
-        <div className="sidebar__top">
-            <h3>Titulo Top</h3>
-            <ExpandMoreIcon/>
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Titulo Top</h3>
+        <ExpandMoreIcon />
+      </div>
+      <div className="sidebar__channels">
+        <div className="sidebar__channels__header">
+            <div className="sidebar__header">
+                <ExpandMoreIcon />
+                <h4>Canales</h4>
+            </div>
+            <AddIcon className="sidebar__addChannel"/>
         </div>
+        <div className="sidebar__channels_list">
+            <SidebarChannel/>
+            <SidebarChannel/>
+            <SidebarChannel/>
+            <SidebarChannel/>
+        </div>
+      </div>
     </div>
-
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
---
+++ src/components/SidebarChannel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../features/SidebarChannel.scss'
+
+function SidebarChannel({id, channel}) {
+  return (
+    <div className='sidebarChannel'>
+        <h4><span className='sidebarChannel__hash'>#</span>Test</h4>
+    </div>
+  )
+}
+
+export default SidebarChannel
