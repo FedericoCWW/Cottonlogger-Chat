c5153966859d1bb7d17ad30d93e0f8df07c58d69
FedericoCWW <federicocandow@hotmail.com>
2025-11-23 20:54:24 -0300

    23_11_2025_2

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import "../features/Chat.scss"
-import ChatHeader from './ChatHeader.js' 
+import React from "react";
+import "../features/Chat.scss";
+import ChatHeader from "./ChatHeader.js";
+import { Input } from "@base-ui-components/react/input";
+import { Button } from '@base-ui-components/react/button';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 function Chat() {
   return (
-    <div className='chat'>
-        <ChatHeader/>
-        <h2>Textoooo</h2>
+    <div className="chat">
+      <ChatHeader />
+      <div className="chat__msgs"></div>
+      <div className="chat__input">
+        <form action="">
+            <AddCircleIcon/>
+            <Input
+                slotProps={{ input: { className: "CustomInputIntroduction" } }}
+                aria-label="Demo input"
+                placeholder="Type something…"
+            />
+            <Button className="chat__submitBtn">Submit</Button>
+        </form>
+        <div className="chat__inputIcons">
+            
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -1,12 +1,35 @@
-import React from 'react'
-import "../features/ChatHeader.scss"
+import React from "react";
+import "../features/ChatHeader.scss";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import EditLocationAltIcon from "@mui/icons-material/EditLocationAlt";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import SearchIcon from '@mui/icons-material/Search';
+import SendIcon from '@mui/icons-material/Send';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 
 function ChatHeader() {
   return (
-    <div className='ChatHeader'>
+    <div className="ChatHeader">
+      <div className="chatHeader__left">
+        <h3>
+          <span className="chatHeader__hash">#</span>
+          Test Channel
+        </h3>
+      </div>
+      <div className="chatHeader__right">
+        <NotificationsIcon />
+        <EditLocationAltIcon />
+        <PeopleAltIcon/>
 
+        <div className="chatHeader__search">
+            <input placeholder="Search" type="text" />
+            <SearchIcon/>
+        </div>
+        <SendIcon/>
+        <HelpOutlineIcon/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatHeader
+export default ChatHeader;
