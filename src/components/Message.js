b60d386c16be9eeac3adfabd3ddac5f391f0137f
Federico <133919977+FedericoCWW@users.noreply.github.com>
2025-12-27 18:32:29 -0300

    27-12-2025: Casi completo el sistema, falta todavia los demas sistemas de logeo , borrar canales, y configracion de registro y logeo, configraucion de usuario. para futuro

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,13 +4,14 @@ import Avatar from '@mui/material/Avatar';
 
 
 function Message({message, user, timestamp}) {
-  let timestamp_Conv = timestamp.toDate().toUTCString()
+  let timestamp_Conv = timestamp ? timestamp.toDate().toUTCString() : '';
   return (
     <div className='msg'>
         <Avatar src={user.photo}/>
         <div className="msg__info">
             <h4>{user.displayName}<span className='msg__timestamp'>{timestamp_Conv.slice(4,-3)}</span></h4>
             <p>{message}</p>
+            {console.log(timestamp_Conv)}
         </div>
     </div>
   )
